abcda3c5137dc9b88a75c7bb88d5937ed113bdb4
geonwooPark <white0581@naver.com>
2024-02-20 16:45:38 +0900

    Chore: Tailwind, StoryBook 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      // fontFamily: {
+      //   pretendard: ['var(--font-pretendard)'],
+      // },
+      colors: {
+        primary: {
+          main: '#4C8BFF',
+          strong: '#196AFF',
+          light: '#E5EEFF',
+        },
+        secondary: {
+          main: '#303030',
+          strong: '#000000',
+        },
+        gray: '#A9A9A9',
+      },
+    },
   },
   // plugins: [require('@tailwindcss/forms')],
-};
+}

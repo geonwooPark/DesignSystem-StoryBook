3da7685cd0fcc0cda0c267366c139486b6065cb0
geonwooPark <white0581@naver.com>
2024-02-21 14:59:00 +0900

    Feat: Modal 컴포넌트 제작

---
+++ src/stories/Modal/Modal.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Modal from './Modal'
+import useModalState from '../../hooks/useModalState'
+
+export default {
+  title: 'MODAL/Modal',
+  component: Modal,
+  tags: ['autodocs'],
+  decorators: [
+    (Story: any) => (
+      <div>
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export function All() {
+  const {
+    isOpen1,
+    isOpen2,
+    openModal1,
+    openModal2,
+    closeModal1,
+    closeModal2,
+    modalData1,
+    modalData2,
+  } = useModalState()
+
+  return (
+    <div>
+      <button onClick={openModal1} className="mr-4 bg-blue-100 px-4 py-2">
+        Type1
+      </button>
+      <button onClick={openModal2} className="bg-red-100 px-4 py-2">
+        Type2
+      </button>
+      <Modal
+        isOpen={isOpen1}
+        closeModal={closeModal1}
+        actionLabel={'확인'}
+        isCancleBtn={false}
+        modalData={modalData1}
+      />
+      <Modal
+        isOpen={isOpen2}
+        closeModal={closeModal2}
+        actionLabel={'삭제'}
+        isCancleBtn={true}
+        modalData={modalData2}
+      />
+    </div>
+  )
+}

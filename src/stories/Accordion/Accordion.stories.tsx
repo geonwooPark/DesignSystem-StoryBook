464637297ba5933e6273901c4cbfc31b7b6fa3ed
kangkyeongseo <kks_big@naver.com>
2024-02-21 14:42:07 +0900

    Feat: Accordion 스토리북 제작

---
+++ src/stories/Accordion/Accordion.stories.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Accordion from './Accordion'
+
+export default {
+  title: 'ACCORDION/Accordion',
+  component: Accordion,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  args: {
+    list: [
+      {
+        title: 'title #1',
+        content: (
+          <p>
+            Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...
+          </p>
+        ),
+      },
+      {
+        title: 'title #2',
+        content: (
+          <p>
+            Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+            Text...Dummy Text...Dummy Text...
+          </p>
+        ),
+      },
+    ],
+  },
+}
+
+export function Open() {
+  const list = [
+    {
+      title: 'title #1',
+      content: (
+        <p>
+          Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...
+        </p>
+      ),
+    },
+    {
+      title: 'title #2',
+      content: (
+        <p>
+          Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
+          Text...Dummy Text...
+        </p>
+      ),
+    },
+  ]
+  return (
+    <>
+      <Accordion list={list} openTitle="title #1" />
+    </>
+  )
+}

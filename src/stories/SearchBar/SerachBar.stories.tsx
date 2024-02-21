8f4e71631b591c9050a8e2f540aa77c73d581354
kangkyeongseo <kks_big@naver.com>
2024-02-21 12:12:27 +0900

    Feat: SearchBar 스토리북 제작

---
+++ src/stories/SearchBar/SerachBar.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+export default {
+  title: 'SEARCHBAR/SearchBar',
+  component: SearchBar,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  args: {
+    placeholder: '검색어를 입력하세요.',
+    onSearchClick: () => null,
+  },
+}

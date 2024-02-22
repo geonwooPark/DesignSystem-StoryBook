df7a43b174c03600c47cf0c0f9930b5fb5dd3024
kangkyeongseo <kks_big@naver.com>
2024-02-22 13:53:12 +0900

    Feat: CheckBox 스토리북 제작

---
+++ src/stories/CheckBox/CheckBox.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import CheckBox from './CheckBox'
+
+const sampleList = [
+  { label: 'Box #1', isChecked: false },
+  { label: 'Box #2', isChecked: false },
+  { label: 'Box #3', isChecked: false },
+  { label: 'Box #4', isChecked: false },
+  { label: 'Box #5', isChecked: false },
+]
+
+export default {
+  title: 'CHECKBOX/CheckBox',
+  component: CheckBox,
+  tags: ['autodocs'],
+}
+
+export const Example = {
+  args: {
+    list: sampleList,
+    changeState: () => null,
+    name: 'checkbox',
+  },
+}
+
+export function DefaultChecked() {
+  const checkedlist = [
+    { label: 'Box #1', isChecked: true },
+    { label: 'Box #2', isChecked: false },
+    { label: 'Box #3', isChecked: false },
+    { label: 'Box #4', isChecked: false },
+    { label: 'Box #5', isChecked: false },
+  ]
+
+  return (
+    <CheckBox list={checkedlist} name={'checkbox'} changeState={() => null} />
+  )
+}

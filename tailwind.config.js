dbbd8b22cb92c0ec86b1a587b836174e7894c8c9
geonwooPark <white0581@naver.com>
2024-02-23 14:41:54 +0900

    Fix: 라이브러리 사용 시 스타일 충돌 해결 및 preset 파일 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  presets: [require('./tw-config/preset')],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      // fontFamily: {
-      //   pretendard: ['var(--font-pretendard)'],
-      // },
-      colors: {
-        primary: {
-          main: '#4C8BFF',
-          strong: '#196AFF',
-          light: '#E5EEFF',
-        },
-        secondary: {
-          main: '#303030',
-          strong: '#000000',
-        },
-        gray: '#A9A9A9',
-      },
-    },
+    extend: {},
   },
-  // plugins: [require('@tailwindcss/forms')],
+  plugins: [],
 }
---
+++ tw-config/preset.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          main: '#4C8BFF',
+          strong: '#196AFF',
+          light: '#E5EEFF',
+        },
+        secondary: {
+          main: '#303030',
+          strong: '#000000',
+        },
+        grey: '#A9A9A9',
+      },
+    },
+  },
+  plugins: [],
+}
